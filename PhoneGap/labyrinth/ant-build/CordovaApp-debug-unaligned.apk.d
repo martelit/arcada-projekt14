09e66d1bf1ba84687d04c39fc65703878a0a12cd
Marko <marko.bjorninen@gmail.com>
2014-11-05 15:30:04 +0200

    Test

---
+++ PhoneGap/labyrinth/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\ant-build\CordovaApp.ap_ \
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\ant-build\classes.dex \
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\CordovaLib\ant-build\classes.jar \
---
+++ PhoneGap/labyrinth/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\ant-build\classes.dex : \
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\ant-build\classes\com\labyrinth\BuildConfig.class \
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\ant-build\classes\com\labyrinth\CordovaApp.class \
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\ant-build\classes\com\labyrinth\R$attr.class \
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\ant-build\classes\com\labyrinth\R$drawable.class \
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\ant-build\classes\com\labyrinth\R$string.class \
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\ant-build\classes\com\labyrinth\R$xml.class \
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\ant-build\classes\com\labyrinth\R.class \
+C:\Program Files\ADT\workspace\labyrinth\platforms\android\ant-build\dexedLibs\classes-16db18830f67d9f6add75b134a2abec8.jar \
