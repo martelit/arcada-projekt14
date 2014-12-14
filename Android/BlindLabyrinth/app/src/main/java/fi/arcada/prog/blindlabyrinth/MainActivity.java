7dd6b2115e41b10d74a3526c60d8017d848dc985
unknown <nybergmi@arcada.fi>
2014-12-14 21:31:13 +0200

    True fullscreen mode added for both main menu and settings.

--- Android/BlindLabyrinth/app/src/main/java/fi/arcada/prog/blindlabyrinth/MainActivity.java
+++ Android/BlindLabyrinth/app/src/main/java/fi/arcada/prog/blindlabyrinth/MainActivity.java
@@ -12,6 +12,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -51,6 +53,10 @@ public class MainActivity extends GameActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
         //For setting background.
--- Android/BlindLabyrinth/app/src/main/java/fi/arcada/prog/blindlabyrinth/Settings.java
+++ Android/BlindLabyrinth/app/src/main/java/fi/arcada/prog/blindlabyrinth/Settings.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
@@ -23,6 +25,10 @@ public class Settings extends GameActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_settings);
 
         SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
